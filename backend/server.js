0b8af448f30bce3a63fac3a29d4673c07faf1657
Toan <maristran72@gmail.com>
2025-02-21 09:16:14 +0700

    21/2 s2

---
+++ backend/server.js
@@ -0,0 +1,26 @@
+require('dotenv').config();
+const express = require('express');
+const mysql = require('mysql2');
+const cors = require('cors');
+const bodyParser = require('body-parser');
+
+const app = express();
+app.use(cors());
+app.use(bodyParser.json());
+
+// Kết nối MySQL
+const db = mysql.createConnection({
+    host: process.env.DB_HOST,
+    user: process.env.DB_USER,
+    password: process.env.DB_PASSWORD,
+    database: process.env.DB_DATABASE,
+    port: process.env.DB_PORT,
+});
+
+db.connect(err => {
+    if (err) {
+        console.error(' Lỗi kết nối MySQL:', err);
+        return;
+    }
+    console.log(' Kết nối MySQL thành công!');
+});
